abd2ce8000ce7fa003f311af5f9c193a6f7381fb
ResGul <reshadgulam@gmail.com>
2020-09-30 22:13:51 +0200

    123

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+    this.state = { items: [] };
+  }
+
+  addNewItem(event) {
+    event.preventDefault();
+    this.setState({
+      items: [...this.state.items, { title: this.inputRef.current.value }]
+    })
+    this.inputRef.current.value = '';
+  }
+  
+  render() {
+    let items = this.state.items.map((item, index) => <li key = {index}>{item.title}</li>);
+
+    return (
+      <div className="App">
+        <form onSubmit={this.addNewItem.bind(this)}>
+          <input ref={this.inputRef}></input>
+          <button>Добавить</button>
+        </form>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
